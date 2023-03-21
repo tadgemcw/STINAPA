0ecca98d9c97a92ff80125d98bd2e4cc223caed9
tadgemcw <tadge.mcwilliams@gmail.com>
2023-03-21 11:47:08 -0400

    Created average water temp plot.

--- TempLight.R
+++ TempLight.R
@@ -8,7 +8,7 @@ tl2021 <- read_excel("2021 raw.xlsx", sheet = 1)
 tl2022 <- read_excel("2022 raw.xlsx", sheet = 1)
 
 
-### Overview of data collected
+############### Overview of data collected  #################
 tl2020 %>% group_by(depth_m) %>%
   ggplot(aes(site, fill = factor(depth_m))) + geom_bar(position = "dodge") +
   xlab("Location") +
@@ -30,7 +30,7 @@ tl2022 %>% group_by(depth_m) %>%
   labs(fill = "Depth (m)") +
   ggtitle("2022 Data Collected")
 
-########### Reviewing 2020 Temp Data for Outliers
+######## Reviewing 2020 Temp Data for Outliers #################
 summary(tl2020$temp_c)
 
 ggplot(tl2020, aes(temp_c)) + 
@@ -76,7 +76,7 @@ ggplot(tl2020, aes(x=factor(month(date)), y = temp_c)) +
 
 tl2020_33 <- tl2020 %>% filter(temp_c <= 33)
   
-#################### Reviewing 2021 Temp Data for Outliers
+########### Reviewing 2021 Temp Data for Outliers  #################
 summary(tl2021$temp_c)
 
 ggplot(tl2021, aes(temp_c)) + 
@@ -122,7 +122,7 @@ tl2021 %>% filter(month(date) == 9 & temp_c < 40) %>%
 
 tl2021_33 <- tl2021 %>% filter(temp_c <= 33)
 
-############## Reviewing 2022 Temp Data for Outliers
+######## Reviewing 2022 Temp Data for Outliers  #################
 summary(tl2022$temp_c)
 
 ggplot(tl2022, aes(temp_c)) + 
@@ -177,7 +177,7 @@ tl2022 %>% filter(temp_c < 15) %>% group_by(date, depth_m, site) %>%
 
 tl2022_33 <- tl2022 %>% filter(temp_c <= 33)
 
-#### Simplifying Dates and Times
+############ Simplifying Dates and Times ################
 tl2020_33$date <- as.Date(tl2020_33$date)
 tl2020_33$time <- format(as.POSIXct(
   tl2020_33$time),format = "%H:%M:%S")
@@ -185,12 +185,41 @@ tl2020_33$time <- format(as.POSIXct(
 
 
 
-#### monthly temp avgs.  Need to deal with outliers.
-tl2020_33 %>%
+################# monthly temp avgs. #################
+avg_temp20 <- tl2020_33 %>%
   mutate(month = month(date)) %>% 
   group_by(month) %>%
-  summarize(mean = mean(temp_c))
+  summarize(avg_temp20 = mean(temp_c))
   
+avg_temp21 <- tl2021_33 %>%
+  mutate(month = month(date)) %>% 
+  group_by(month) %>%
+  summarize(avg_temp21 = mean(temp_c))
+
+avg_temp22 <- tl2022_33 %>%
+  mutate(month = month(date)) %>% 
+  group_by(month) %>%
+  summarize(avg_temp22 = mean(temp_c))
+
+avg_temps <- avg_temp20 %>% 
+  full_join(avg_temp21, by = "month") %>%
+  full_join(avg_temp22, by = "month") %>%
+  arrange(month)
+avg_temps
+
+avg_temps %>% 
+  pivot_longer(cols = avg_temp20:avg_temp22, names_to = "year", values_to = "temp") %>% 
+  ggplot(aes(month,  temp, color = year)) +
+    geom_line() +
+    scale_color_manual(name = "Year", 
+                       labels = c("2020", "2021", "2022"),
+                       values = c("deepskyblue1","darkviolet","darkblue"),
+                       guide = guide_legend(reverse = TRUE)) +
+    scale_x_continuous(breaks = 1:12, "Month") + 
+    scale_y_continuous(limits = c(23, 31), breaks = seq(23, 31, 1), "Water Temperature (c)") + 
+    ggtitle("Average Water Temperature") 
+
+
 
 ### Reviewing Light Data
 summary(tl2020$light)
